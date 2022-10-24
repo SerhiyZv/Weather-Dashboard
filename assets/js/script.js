cc953aedd0f2a81d12b622f6364604b5694203ca
SerhiyZv <skiev31@gmail.com>
2022-10-23 23:48:30 -0400

    Update HTML and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,67 @@
 var APIKey = "ca3339aa147dcdad470993efa11f2132"; // default key
 var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-const citySearchInput = document.querySelector("#searchcity");
-const searchForm = document.querySelector("#search");
+const citySearchInput = document.querySelector("#city-name");
+const searchForm = document.querySelector("#search-form");
+const currentConditionsUl = document.querySelector("#current-weather #conditions");
+const currentConditionsH3 = document.querySelector("#current-weather h3")
 const previousSearches = document.querySelector("#previous-searches");
+
+let searchValue = "";
 
 const localCityArray = [];
 
-let previousSearch = JSON.parse(localStorage.getItem("searches"));
+const updateSearchHistory = () => {
+    const previousSearch = JSON.parse(localStorage.getItem("searches"));
 
-if (previousSearch !== null) {
-    for (let i = 0; i < previousSearch.length; i++) {
-        if (previousSearch[i] === null) {
-            previousSearch.splice(i, i+1);
+    if (previousSearch === null) {
+        previousSearches.classList.add("hidden");
+    }else {
+        previousSearches.classList.remove("hidden");
+        const existingButtons = document.querySelectorAll("#previous-searches button");
+
+        if(existingButtons.length === 0) {
+            for (let i = 0; i < previousSearch.length; i++) {
+                const searchButton = document.createElement("button");
+                searchButton.classList.add("btn");
+                searchButton.dataset.city = previousSearch[i];
+                searchButton.textContent = previousSearch[i];
+                searchButton.addEventListener("click", (event) => {
+                    callOpenWeather(event.target.dataset.city);
+                })
+                document.querySelector("#previous-searches .card-body").appendChild(searchButton);
+            }
         }else {
-            localCityArray.push(previousSearch[i]);
+            existingButtons.forEach(button => {
+                for (let i = 0; i < previousSearch.length; i++)
+                if (button.dataset.city.includes(previousSearch[i])) {
+                    previousSearch.splice(i, i +1);
+                }
+            })
+            for (let i = 0; i < previousSearch.length; i++) {
+                const searchButton = document.createElement("button");
+                searchButton.classList.add("btn");
+                searchButton.dataset.city = previousSearch[i];
+                searchButton.textContent = previousSearch[i];
+                searchButton.addEventListener("click", (event) => {
+                    callOpenWeather(event.target.dataset.city);
+                })
+                document.querySelector("#previous-searches .card-body").appendChild(searchButton);
+            }
         }
+    }    
+}
+
+const updateLocalStorage = (city) => {
+    if (localCityArray.includes(city)) {
+        return;
+    }else {
+        localCityArray.push(city);
+
+        localStorage.setItem("searches", JSON.stringify(localCityArray));
+
+        updateSearchHistory();
     }
 }
+
+
