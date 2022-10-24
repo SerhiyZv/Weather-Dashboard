083d9f7b475ae1ebf1bad2cd398b1c629339d509
SerhiyZv <skiev31@gmail.com>
2022-10-23 21:46:49 -0400

    Update HTML with header and Search and 5 Days Forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 var APIKey = "ca3339aa147dcdad470993efa11f2132"; // default key
 var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+const citySearchInput = document.querySelector("#searchcity");
+const searchForm = document.querySelector("#search");
+const previousSearches = document.querySelector("#previous-searches");
 
+const localCityArray = [];
+
+let previousSearch = JSON.parse(localStorage.getItem("searches"));
+
+if (previousSearch !== null) {
+    for (let i = 0; i < previousSearch.length; i++) {
+        if (previousSearch[i] === null) {
+            previousSearch.splice(i, i+1);
+        }else {
+            localCityArray.push(previousSearch[i]);
+        }
+    }
+}
