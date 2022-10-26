d3422f8925e7c275bbf8522c4c871fe0c9c6f820
SerhiyZv <skiev31@gmail.com>
2022-10-25 21:50:53 -0400

    Fix UVI index and Update fetch API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,17 @@
 // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 const cityNameInput = document.querySelector("#city-name");
 const searchForm = document.querySelector("#search-form");
-const currentConditionsUl = document.querySelector("#current-weather #conditions");
-const currentConditionsH3 = document.querySelector("#current-weather h3")
+const currentConditionsUl = document.querySelector("#current-forecast #conditions");
+const currentConditionsH3 = document.querySelector("#current-forecast h3")
 const previousSearches = document.querySelector("#previous-searches");
+const previousSearchContainer = document.querySelector("#previous-searches .card-body");
+const dailyCardContainer = document.querySelector("#daily-forecast");
+const fiveDayHeader = document.querySelector("#five-day");
 
-let searchValue = "";
 
 const localCityArray = [];
 
-const previousSearch = JSON.parse(localStorage.getItem("searches"));
+let previousSearch = JSON.parse(localStorage.getItem("searches"));
 
 if (previousSearch !== null) {
     for (let i = 0; i < previousSearch.length; i++) {
@@ -24,45 +26,31 @@ if (previousSearch !== null) {
 }
 
 const updateSearchHistory = () => {
-    const previousSearch = JSON.parse(localStorage.getItem("searches"));
+     previousSearch = JSON.parse(localStorage.getItem("searches"));
 
-    if (previousSearch === null) {
-        previousSearches.classList.add("hidden");
-    }else {
-        previousSearches.classList.remove("hidden");
-        const existingButtons = document.querySelectorAll("#previous-searches button");
+     const existingButtons = document.querySelectorAll("#previous-searches button");
 
-        if(existingButtons.length === 0) {
-            for (let i = 0; i < previousSearch.length; i++) {
-                const searchButton = document.createElement("button");
-                searchButton.classList.add("btn");
-                searchButton.dataset.city = previousSearch[i];
-                searchButton.textContent = previousSearch[i];
-                searchButton.addEventListener("click", (event) => {
-                    callOpenWeather(event.target.dataset.city);
-                })
-                document.querySelector("#previous-searches .card-body").appendChild(searchButton);
+    if (previousSearch !== null) {
+        existingButtons.forEach(button => {
+            for (let i =0; i < previousSearch.length; i++)
+            if (button.dataset.city.includes(previousSearch[i])) {
+                previousSearch.splice(i, i + 1);
             }
-        }else {
-            existingButtons.forEach(button => {
-                for (let i = 0; i < previousSearch.length; i++)
-                if (button.dataset.city.includes(previousSearch[i])) {
-                    previousSearch.splice(i, i +1);
-                }
-            })
+        })
+
             for (let i = 0; i < previousSearch.length; i++) {
                 const searchButton = document.createElement("button");
-                searchButton.classList.add("btn");
+                searchButton.classList.add("m-2", "btn", "btn-light");
                 searchButton.dataset.city = previousSearch[i];
                 searchButton.textContent = previousSearch[i];
                 searchButton.addEventListener("click", (event) => {
                     callOpenWeather(event.target.dataset.city);
                 })
-                document.querySelector("#previous-searches .card-body").appendChild(searchButton);
+                previousSearchContainer.appendChild(searchButton);
             }
         }
     }    
-}
+
 
 const updateLocalStorage = (city) => {
     console.log(localCityArray);
@@ -84,13 +72,14 @@ const callOpenWeather = (city) => {
     
     fetch(apiUrlCoords)
     .then(function (response) {
-        if(!response.ok) {
+        if (!response.ok) {
             currentConditionsUl.innerHTML = "";
             currentConditionsH3.textContent = "Try again!";
             const errorText = document.createElement("li");
             errorText.textContent = "City not found.";
             currentConditionsUl.appendChild(errorText);
-            throw Error(response.statusText);
+            dailyCardContainer.innerHTML = "";
+            fiveDayHeader.classList.add("hidden");
         } else {
             response.json()
         .then(function (data) {
@@ -98,7 +87,8 @@ const callOpenWeather = (city) => {
 
             const cityName = data.name;
 
-            const oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=ca3339aa147dcdad470993efa11f2132";
+            const oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=ca3339aa147dcdad470993efa11f2132`;
+
             fetch(oneCallUrl)
             .then(function (response) {
                 if (response.ok) {
@@ -106,30 +96,69 @@ const callOpenWeather = (city) => {
                     .then(function (data){
                         console.log(data);
 
-                        const icon = ("<img src='http://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png'>");
+                        const icon = ("<img src='http://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png' alt='Weather icon>");
                         currentConditionsH3.innerHTML = cityName + " (" + moment().format("MM/DD/YYYY") + ") " + icon;
         
                 const liArray = [];
 
                 currentConditionsUl.innerHTML = ""
 
-                for (let i =0; i < 4; i++) {
-                    liArray.push(document.createElement("li"));
+                for (let i = 0; i < 4; i++) {
+                    const li = document.createElement("li");
+                    li.classList.add("mb-2");
+                    liArray.push(li);
                 }
 
                 liArray[0].innerHTML = "Temperature: " + data.current.temp + " &deg;F";
                 liArray[1].textContent = "Humidity: " + data.current.humidity + "%";
                 liArray[2].textContent = "Wind Speed: " + data.current.wind_speed + " MPH";
-                liArray[3].textContent = "UV Index: " + data.current.uvi;
+                
+                const uvi = Math.floor(data.current.uvi);
+
+                // Evaluation to populate UV Index color based on value
+                if (uvi <= 2) {
+                    liArray[3].innerHTML = `UV Index: <button class="btn btn-info uv">${uvi}</button>`;
+                } else if (uvi > 2 && uvi <= 5) {
+                    liArray[3].innerHTML = `UV Index: <button class="btn btn-success uv">${uvi}</button>`;
+                } else if (uvi > 5 && uvi <= 8) {
+                    liArray[3].innerHTML = `UV Index: <button class="btn btn-warning uv">${uvi}</button>`;
+                } else {
+                    liArray[3].innerHTML = `UV Index: <button class="btn btn-danger uv">${uvi}</button>`;
+                }
 
                 liArray.forEach(li => {
                     currentConditionsUl.append(li);
                 })
 
+                let dailyArray = [];
+
+                dailyCardContainer.innerHTML = "";
+
+                for (let i = 0; i < 5; i++) {
+                    const dailyCard = document.createElement("div");
+                    dailyCard.innerHTML = `
+                    <div class="p-2 m-2 card bg-info text-white">
+                        <h5>${moment().add(i + 1, "days").format("MM/DD/YYYY")}</h5>
+                        <ul id="conditions">
+                            <li><img src='http://openweathermap.org/img/w/${data.daily[i].weather[0].icon}.png' alt="Weather icon" class="mx-auto"></li>
+                            <li>Temp: ${Math.floor(data.daily[i].temp.day)} &deg;F</li>
+                            <li>Humidity: ${data.daily[i].humidity}%</li>
+                        </ul>
+                    </div>`;
+
+                    dailyArray.push(dailyCard);
+                }
+
+                fiveDayHeader.classList.remove("hidden");
+
+                dailyArray.forEach(card => {
+                    dailyCardContainer.appendChild(card);
+                })
+
                 updateLocalStorage(cityName);
             })
         }
-    })
+        })
     })
 }
 })
@@ -138,9 +167,13 @@ const callOpenWeather = (city) => {
 searchForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    searchValue = cityNameInput.value.trim("");
+    let searchValue = cityNameInput.value.trim("");
+
     if (searchValue === "") {
         currentConditionsH3.textContent = "Please enter a city!";
+        currentConditionsUl.innerHTML = "";
+        dailyCardContainer.innerHTML = "";
+        fiveDayHeader.classList.add("hidden");
     }else {
         callOpenWeather(searchValue);
         cityNameInput.value = "";
